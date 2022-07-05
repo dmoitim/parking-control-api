9437522411359cf154093e1bfb8d3d3977d8a956
Nome <email@email.com>
2022-07-05 18:57:46 -0300

    Criação do método de inserção

--- src/main/java/br/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/br/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,10 +1,17 @@
 package br.com.api.parkingcontrol.controllers;
 
+import br.com.api.parkingcontrol.dtos.ParkingSpotDto;
+import br.com.api.parkingcontrol.models.ParkingSpotModel;
 import br.com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -12,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParkingSpotController {
     @Autowired
     ParkingSpotService parkingSpotService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
 }
--- src/main/java/br/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/br/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,11 +1,19 @@
 package br.com.api.parkingcontrol.services;
 
+import br.com.api.parkingcontrol.models.ParkingSpotModel;
 import br.com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
     @Autowired
     ParkingSpotRepository parkingSpotRepository;
+
+    @Transactional
+    public Object save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
 }
